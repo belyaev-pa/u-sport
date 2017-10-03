ccbb6952be56dcff097d01b0f18127315c4f8c04
belyaev-pa <32476417+belyaev-pa@users.noreply.github.com>
2017-10-03 10:49:20 +0400

    Update urls.py

--- u-sport/urls.py
+++ u-sport/urls.py
@@ -1,1 +1,13 @@
+from django.conf.urls import include, url
+from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
+from . import views
 
+urlpatterns = [    
+  url(r'^admin/', include(admin.site.urls)),
+  url(r'^admin/doc/', include('django.contrib.admindocs.urls')),    
+  url(r'^$', views.stest_redirect, name="stest_redirect"),    
+  url(r'^main/', include('main.urls', namespace="main")),    
+  url(r'^auth/', include('django.contrib.auth.urls', namespace='auth')),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
